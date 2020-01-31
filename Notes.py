3f9738be12825ff9fe3cc48123f3746f3aedb005
gaurishankar007 <gaurisharma358@gmail.com>
2020-01-31 07:42:58 -0800

    Notes for exams

---
+++ Notes.py
@@ -0,0 +1,11 @@
+a = int(13 / 2)
+print(a)  # 6
+print(int(13.5 / 2))  # 6
+
+print(float(13.5 / 2))  # 6.75
+
+print(round(6.5))  # 6
+print(round(6.49))  # 6
+print(round(6.51))  # 7
+
+print(33 == 33.0)  # True
