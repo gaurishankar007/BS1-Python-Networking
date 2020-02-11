fe8e067eb7b348d1cb5fa2267065ee1afdb91a44
gaurishankar007 <gaurisharma358@gmail.com>
2020-02-11 15:56:02 -0800

    Correcting Lab exercise 2

--- Lab_exercise_1&2/Answer of Lab exercise 2..py
+++ Lab_exercise_1&2/Answer of Lab exercise 2..py
@@ -42,12 +42,12 @@ else:
 a = int(input("Enter the first number."))
 b = int(input("Enter the second number."))
 c = int(input("Enter the third number."))
-if a > b and a > c:
-    print(f"{a} is the greatest number")
-elif b > a and b > c:
-    print(f"{b} is the greatest number")
+if a < b and a < c:
+    print(f"{a} is the smallest number.")
+elif b < a and b < c:
+    print(f"{b} is the smallest number.")
 else:
-    print(f"{c} is the greatest number")
+    print(f"{c} is the smallest number.")
 
 # 5.
 a = int(input("Enter the given number"))
